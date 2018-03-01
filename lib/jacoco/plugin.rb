31b5b471a2fdb80ddf6e6512f298a56822ca8118
Antony Alkmim <antony-alkmim@hotmail.com>
2018-03-01 11:35:46 -0300

    Add support for Kotlin files and improve Pull Request Report

--- lib/jacoco/plugin.rb
+++ lib/jacoco/plugin.rb
@@ -1,4 +1,5 @@
 require 'jacoco/sax_parser'
+require "oga"
 
 module Danger
   #
@@ -6,7 +7,16 @@ module Danger
   # @tags jacoco, coverage, java, android
   #
   class DangerJacoco < Plugin
-    attr_accessor :minimum_coverage_percentage
+    attr_accessor :minimum_project_coverage_percentage
+    attr_accessor :minimum_class_coverage_percentage
+    attr_accessor :files_extension
+
+    def setup
+      @minimum_project_coverage_percentage = 60 unless minimum_project_coverage_percentage
+      @minimum_class_coverage_percentage = 75 unless minimum_class_coverage_percentage
+      @files_extension = [".kt",".java"] unless files_extension
+    end
+
 
     # Parses the xml output of jacoco to Ruby model classes
     # This is slow since it's basically DOM parsing
@@ -32,46 +42,78 @@ module Danger
     # blah/blah/java/slashed_package/Source.java
     #
     def report(path, delimiter = '/java/')
+      setup
       classes = classes(delimiter)
-
+      # puts delimiter
       parser = Jacoco::SAXParser.new(classes)
       Nokogiri::XML::SAX::Parser.new(parser).parse(File.open(path))
+      
+      total_covered = total_coverage(path)
 
-      parser.classes.each do |jacoco_class|
-        # Check which metrics are available
-        report_class(jacoco_class)
+      report_markdown = "### JaCoCO Code Coverage #{total_covered[:covered]}% #{total_covered[:status]}\n"
+      report_markdown << "| Classe | Coverage Atual | Meta | Status |\n"
+      report_markdown << "|:---:|:---:|:---:|:---:|\n"
+      parser.classes.each do |jacoco_class| # Check metrics for each classes
+        rp = report_class(jacoco_class)
+        report_markdown << "| `#{jacoco_class.name}` | #{rp[:covered]}% | #{minimum_class_coverage_percentage}% | #{rp[:status]} |\n"
+      end  
+      markdown(report_markdown)
+
+      # fail danger if total coveraged is smaller than minimum_project_coverage_percentage
+      if total_covered[:covered] < minimum_project_coverage_percentage
+        fail("Total coverage of #{total_covered[:covered]}%. Improve this to as least #{minimum_project_coverage_percentage}")
       end
     end
 
+    # Select modified and added files in this PR
     def classes(delimiter)
       git = @dangerfile.git
       affected_files = git.modified_files + git.added_files
-      affected_files.select { |file| file.end_with? '.java' }
-                    .map { |file| extract_class(file, delimiter) }
+      affected_files.select { |file| files_extension.reduce(false) { |state, el| state or file.end_with?(el) } }
+                    .map { |file| file.split(".").first.split(delimiter)[1] }
     end
 
+    # It returns a specific class code coverage and an emoji status as well
     def report_class(jacoco_class)
+      
       counters       = jacoco_class.counters
       branch_counter = counters.detect { |e| e.type.eql? 'BRANCH' }
       line_counter   = counters.detect { |e| e.type.eql? 'LINE' }
       counter        = branch_counter.nil? ? line_counter : branch_counter
-
-      report_counter(counter, jacoco_class)
+      coverage = (counter.covered.fdiv(counter.covered + counter.missed) * 100).floor
+      status = coverage_status(coverage, minimum_class_coverage_percentage)
+      
+      return {
+        covered: coverage,
+        status: status
+      }
+      
     end
 
-    def report_counter(counter, jacoco_class)
-      covered  = counter.covered
-      missed   = counter.missed
-      coverage = (covered.fdiv(covered + missed) * 100).floor
-
-      return unless coverage < minimum_coverage_percentage
-
-      fail("#{jacoco_class.name} has coverage of #{coverage}%. " \
-              "Improve this to at least #{minimum_coverage_percentage}%")
+    # it returns an emoji for coverage status
+    def coverage_status(coverage, minimum_percentage)
+      return case
+      when coverage < (minimum_percentage/2);  ":skull:"
+      when coverage < minimum_percentage;  ":warning:"
+      else ":white_check_mark:"
+      end
     end
 
-    def extract_class(file, java_path_delimiter)
-      file[0, file.length - 5].split(java_path_delimiter)[1]
+    # It returns total of project code coverage and an emoji status as well
+    def total_coverage(report_path)
+        jacoco_report = Oga.parse_xml(File.open(report_path))
+        
+        report = jacoco_report.xpath('report/counter').select { |item| item.get('type') == "INSTRUCTION" }
+        missed_instructions = report.first.get('missed').to_f
+        covered_instructions = report.first.get('covered').to_f
+        total_instructions = missed_instructions + covered_instructions
+        covered_percentage = (covered_instructions * 100 / total_instructions).round(2)
+        coverage_status = coverage_status(covered_percentage, minimum_project_coverage_percentage)
+
+        return {
+          covered: covered_percentage,
+          status: coverage_status
+        }
     end
   end
 end
